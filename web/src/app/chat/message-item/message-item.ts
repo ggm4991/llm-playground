3f8d0efab9b26c55c4c6bc442d037be8ccc02bb3
Gonzalo <goncholoko@gmail.com>
2026-09-25 18:42:41 +0200

    feat(web): ver más/ver menos en MessageItem.

--- web/src/app/chat/message-item/message-item.ts
+++ web/src/app/chat/message-item/message-item.ts
@@ -1,29 +1,42 @@
-import { Component, computed, input, output } from '@angular/core';
+import { Component, computed, input, linkedSignal, output } from '@angular/core';
 import { ChatMessage } from '../chat-message';
 
 @Component({
   selector: 'app-message-item',
   styleUrl: './message-item.scss',
-  templateUrl: './message-item.html',
+  templateUrl: './message-item.html'
 })
 export class MessageItem {
   readonly message = input.required<ChatMessage>();
   readonly maxLength = input(200);
   readonly copyRequested = output<string>();
+  
 
+  protected readonly isExpanded = linkedSignal({
+    source: () => this.message(),
+    computation: () => false,
+  });
+
+  protected toggleExpanded() {
+    this.isExpanded.update(value => !value);
+  }
 
   protected readonly isUser = computed<boolean>(()=>{
     return this.message().role === 'user';
   });
+
+  protected readonly isMaxLength = computed(()=>{
+    return this.message().content.length > this.maxLength();
+  });
   
   protected readonly preview = computed(()=>{
-    return this.message().content.length > this.maxLength() ? 
+    return !this.isExpanded() && this.isMaxLength() ?
     this.message().content.slice(0, this.maxLength())+"..." : 
     this.message().content;
   });
 
   protected onCopyClick() {
-    this.copyRequested.emit(this.message().content)
+    this.copyRequested.emit(this.message().content);
   }
 
   
